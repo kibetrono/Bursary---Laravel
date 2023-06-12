2b3ed7a1d3790f6af80a5734cf8e5995720ca315
Kibet <kibetdavidro@gmail.com>
2023-06-12 08:08:19 +0300

    create ProfileController

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Profile;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class ProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Profile  $profile
+     * @return \Illuminate\Http\Response
+     */
+    public function show($encryptedId)
+    {
+        $id = decrypt($encryptedId);
+
+        $user = User::findorFail($id);
+
+        return view('profile.show',compact('user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Profile  $profile
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($encryptedId)
+    {
+        $id = decrypt($encryptedId);
+        $user = User::findOrFail($id);
+        
+        // dd($user);
+        return view('profile.edit',compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Profile  $profile
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request,[
+            'name' => 'required|max:120',
+            'email' => 'required|email|unique:users,email,'.$id,
+            'password' => 'nullable|min:6|confirmed',
+        ]);
+
+        $user = User::findOrFail($id);
+
+        $user->name      = $request->name;
+        $user->email      = $request->email;
+        // $user->phone      = $request->phone;
+
+        if($request->has('password') && $request['password'] !== null){
+            $user->password   = Hash::make($request->password);
+        }
+
+
+        $user->save();
+
+        return redirect()->route('profile.show',encrypt($id))->with('success','User updated successfully');
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Profile  $profile
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Profile $profile)
+    {
+        //
+    }
+
+
+}
